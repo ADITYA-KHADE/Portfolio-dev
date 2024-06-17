f5ae8ee52a7aa43811fb314f3c4df4d7ffe71ced
adityakhade321 <adityakhadeak47@gmail.com>
2024-06-18 00:33:48 +0530

    icon uploaded

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const multer = require("multer");
 const cookieParser = require("cookie-parser");
 const fs = require('fs');
 
@@ -22,19 +21,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, uploadsDir);
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, `${file.fieldname}_${Date.now() + path.extname(file.originalname)}`);
-//     },
-// });
-
-// const upload = multer({ storage: storage });
-
-// app.use(upload.single("file"));  it is use to apply globally across all routes.
-
 const authRoute = require("./routes/authRoute");
 const contactRoute = require("./routes/contactRoute");
 const infoRoute = require("./routes/infoRoute");
@@ -46,24 +32,6 @@ app.use("/api/contact", contactRoute);
 app.use("/api/info", infoRoute);
 app.use("/api/project", projectRoute);
 app.use("/api/skill",skillsRoute);
-
-// Route to handle file uploads [only for project photos]
-// app.post("/upload-image", upload.single("file"), (req, res) => {
-//     try {
-//         res.status(200).json({ 
-//             success:1,
-//             image_url:`/image/${req.file.filename}`,
-//             message: 'Image uploaded successfully'
-//         });
-//     } catch (error) {
-//         res.status(400).send("Error uploading file");
-//     }
-// });
-
-// app.get("/image/:filename", (req, res) => {
-//     const { filename } = req.params;
-//     res.sendFile(path.join(__dirname, `uploads/${filename}`));
-// });
 
 app.listen(PORT, () => {
   console.log(`Server is Listening on http://localhost:${PORT}`);
