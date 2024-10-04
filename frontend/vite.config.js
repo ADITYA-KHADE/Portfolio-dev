304d54ad9bb642c461fc4ca04435d3de4b7b12cf
adityakhade321 <adityakhadeak47@gmail.com>
2024-10-04 21:03:48 +0530

    Add vite-plugin-sitemap to package.json and configure in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteSitemap } from 'vite-plugin-sitemap';
+import ViteSitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
+  plugins: [react(),
     ViteSitemap({
-      hostname: 'https://myadityakhade.tech', 
+      hostname: 'https://myadityakhade.tech',
       routes: [
         '/',
         '/admin',
-      ]
-    }),
+      ],
+    })
   ],
   server: {
     port: 3000,
