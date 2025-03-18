dd0c4a03ac8800c83f5c26e4ec471344d016b4bd
adityakhade321 <adityakhadeak47@gmail.com>
2025-03-18 20:23:22 +0530

    Add Firebase integration and analytics logging to the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,12 @@
 import { Toaster } from "react-hot-toast";
 import Portfolio from "./pages/Portfolio";
 import { account, ID } from './lib/appwrite';
+import { analytics } from "./firebaseConfig";
+import { logEvent } from "firebase/analytics";
+import { firestore } from './firebaseConfig'; 
+import { collection, addDoc, getDocs } from 'firebase/firestore';
+
+logEvent(analytics, "notification_received");
 
 
 function App() {
